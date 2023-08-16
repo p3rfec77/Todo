d703b75a811d00dbab84b1b52aaa0f337d8c5598
Kirill Golygin <kirik3007@gmail.com>
2023-08-16 17:40:45 +0300

    created InputField component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React from "react";
+
+import { InputField } from "./components";
+
 import styles from "./App.module.css";
 
 function App() {
-  return <div className={styles.App}>todo</div>;
+  return (
+    <div className={styles.App}>
+      <InputField />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/InputField/InputField.component.tsx
@@ -0,0 +1,36 @@
+import { useAppSelector, useAppDispatch } from "../../hooks/ReduxHooks";
+import { setText, selectText, addTodo } from "../../store/todoSlice";
+
+import styles from "./InputField.module.css";
+
+export const InputField = () => {
+  const text: string = useAppSelector(selectText);
+  const dispatch = useAppDispatch();
+
+  const InputHandler = (e: React.ChangeEvent<HTMLInputElement>) =>
+    dispatch(setText(e.target.value));
+
+  const AddTask = () => {
+    dispatch(addTodo({ text }));
+    dispatch(setText(""));
+  };
+
+  const addTaskByEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      dispatch(addTodo({ text }));
+      dispatch(setText(""));
+    }
+  };
+  return (
+    <>
+      <label>
+        <input
+          value={text}
+          onChange={(e) => InputHandler(e)}
+          onKeyDown={(e) => addTaskByEnter(e)}
+        />
+        <button onClick={AddTask}>Add Todo</button>
+      </label>
+    </>
+  );
+};
